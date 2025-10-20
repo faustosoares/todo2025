2a12f47e38d7cd7beefd33eb0677c6c8e4dea197
Fausto Soares <fausto.ifs@gmail.com>
2025-10-20 11:43:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/fbms/todo/Todo2025Application.java
@@ -0,0 +1,13 @@
+package br.com.fbms.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todo2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todo2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fbms/todo/Todo2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fbms.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todo2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
